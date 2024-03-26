0751a094d8aa99ce0f4b519380f12fbc18c10885
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-03-26 13:35:39 -0700

    1-binary_tree_insert_left.c

--- binary_trees.h
+++ binary_trees.h
@@ -2,14 +2,20 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-
-/* Binary tree node structure */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 
@@ -27,4 +33,3 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
-
